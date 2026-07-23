fb65dd71981447b4bf1d0db8f52bb66b59758dca
thfql7976 <thfql7976@gmail.com>
2026-07-23 18:21:40 +0900

    애니메이션 수정

--- scripts/animation01.js
+++ scripts/animation01.js
@@ -2,19 +2,19 @@
 // item에 마우스를 올리면 img태그 경로가 다른 이미지로 변경
 //변수
 const  item1 = document.querySelector('.item');
-const item1Img = document.querySelector('.item1Img');
+const item1Img = document.querySelector('.item img');
 
 //변수테스트
 console.log(item1, item1Img);
 
 //이벤트
 item1.addEventListener('mouseover',item1ImgSrc);
-item1.addEventListener('mouseover',item1ImgOut);
+item1.addEventListener('mouseout',item1ImgOut);
 
 //함수
 function item1ImgSrc() {
-    item1Img.Src = 'https://d3ha2047wt6x28.cloudfront.net/UX3oyNWTtmI/pr:GOODS_DETAIL/czM6Ly9hYmx5LWltYWdlLWxlZ2FjeS9kYXRhL2dvb2RzLzIwMjUwNTE5XzE3NDc2MTgzOTk1NTA1OTZtLmpwZw';
+    item1Img.src = 'https://d3ha2047wt6x28.cloudfront.net/UX3oyNWTtmI/pr:GOODS_DETAIL/czM6Ly9hYmx5LWltYWdlLWxlZ2FjeS9kYXRhL2dvb2RzLzIwMjUwNTE5XzE3NDc2MTgzOTk1NTA1OTZtLmpwZw';
 }
 function item1ImgOut() {
-    item1Img.Src= 'https://d3ha2047wt6x28.cloudfront.net/UX3oyNWTtmI/pr:GOODS_DETAIL/czM6Ly9hYmx5LWltYWdlLWxlZ2FjeS9kYXRhL2dvb2RzLzIwMjUwNTE5XzE3NDc2MTgzOTk1NTA1OTZtLmpwZw';
+    item1Img.src= 'https://d3ha2047wt6x28.cloudfront.net/VNfZAzl20zE/pr:GOODS_DETAIL/czM6Ly9hYmx5LWltYWdlLWxlZ2FjeS9kYXRhL2dvb2RzLzIwMjUwNTE5XzE3NDc2MTgzOTk2NzgyOTBtLmpwZw';
 }
