0847ebae855aae5ce277c7d14650a2f08911e00b
thfql7976 <thfql7976@gmail.com>
2026-07-23 18:12:58 +0900

    애니메이션 시작

---
+++ scripts/animation01.js
@@ -0,0 +1,20 @@
+// animation01.html
+// item에 마우스를 올리면 img태그 경로가 다른 이미지로 변경
+//변수
+const  item1 = document.querySelector('.item');
+const item1Img = document.querySelector('.item1Img');
+
+//변수테스트
+console.log(item1, item1Img);
+
+//이벤트
+item1.addEventListener('mouseover',item1ImgSrc);
+item1.addEventListener('mouseover',item1ImgOut);
+
+//함수
+function item1ImgSrc() {
+    item1Img.Src = 'https://d3ha2047wt6x28.cloudfront.net/UX3oyNWTtmI/pr:GOODS_DETAIL/czM6Ly9hYmx5LWltYWdlLWxlZ2FjeS9kYXRhL2dvb2RzLzIwMjUwNTE5XzE3NDc2MTgzOTk1NTA1OTZtLmpwZw';
+}
+function item1ImgOut() {
+    item1Img.Src= 'https://d3ha2047wt6x28.cloudfront.net/UX3oyNWTtmI/pr:GOODS_DETAIL/czM6Ly9hYmx5LWltYWdlLWxlZ2FjeS9kYXRhL2dvb2RzLzIwMjUwNTE5XzE3NDc2MTgzOTk1NTA1OTZtLmpwZw';
+}
